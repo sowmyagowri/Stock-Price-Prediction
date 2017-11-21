43c090be1cad053e5fed53d7b793e1de279ca218
sowmyagowri <gsowmya@gmail.com>
2017-11-21 02:27:23 -0800

    Change prediction function

--- Stock Price Prediction/app.py
+++ Stock Price Prediction/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
-#from functions import *
+from functions import *
 import  myexception
 
 app = Flask(__name__)
@@ -25,41 +25,13 @@ def index():
 
 #stock prices trend api
 @app.route('/prediction', methods=['GET', 'POST'])
-def get_historical():
+def get_prediction():
     
     try:
-        quote = request.json['company']
-        # Download our file from google finance
-        url = 'http://www.google.com/finance/historical?q=NASDAQ%3A'+quote+'&output=csv'
-        r = requests.get(url, stream=True)
-    
-        dataset = []
-    
-        with open(FILE_NAME) as f:
-            for n, line in enumerate(f):
-                if n != 0:
-                    str = line.split(',')[1]
-                    if(str != "-"):
-                        dataset.append(float(line.split(',')[1]))
-    
-        dataset = np.array(dataset)
-    
-        # Create dataset matrix (X=t and Y=t+1)
-        dataX = [dataset[n+1] for n in range(len(dataset)-2)]
-        trainX = np.array(dataX)
-        trainY = dataset[2:]
-    
-        # Create and fit Multilinear Perceptron model
-        model = Sequential()
-        model.add(Dense(8, input_dim=1, activation='relu'))
-        model.add(Dense(1))
-        model.compile(loss='mean_squared_error', optimizer='adam')
-        model.fit(trainX, trainY, nb_epoch=200, batch_size=2, verbose=2)
-    
-        # Our prediction for tomorrow
-        prediction = model.predict(np.array([dataset[0]]))
-        result = 'The price will move from %s to %s' % (dataset[0], prediction[0][0])
-        
+        if not request.json or not 'company' in request.json:
+            abort(400)
+        company = request.json['company']
+        result = predict_prices(company)
         return jsonify(result)
     
     except ValidationError as e:
