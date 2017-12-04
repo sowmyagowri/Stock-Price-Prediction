f9a824f5d18131dc54c3d69f42436c1fa95f0334
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2017-12-03 22:10:20 -0800

    Send last original value and first future value to frontend

--- Command Prompt Run/prophet.py
+++ Command Prompt Run/prophet.py
@@ -60,6 +60,7 @@ def main():
         df['ds'] = df.index
         #log transform the ‘Close’ variable to convert non-stationary data to stationary.
         df['y'] = np.log(df['Close'])
+        original_end = df['Close'][-1]
         
         model = Prophet()
         model.fit(df)
@@ -71,8 +72,6 @@ def main():
         forecast = model.predict(future)
         
         print (forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
-
-        plot_num = 365
         
         #Prophet plots the observed values of our time series (the black dots), the forecasted values (blue line) and
         #the uncertainty intervalsof our forecasts (the blue shaded regions).
@@ -88,8 +87,6 @@ def main():
         viz_df = df.join(forecast[['yhat', 'yhat_lower','yhat_upper']], how = 'outer')
         viz_df['yhat_scaled'] = np.exp(viz_df['yhat'])
 
-        
-
         #close_data = viz_df.Close.tail(plot_num)
         #forecasted_data = viz_df.yhat_scaled.tail(plot_num)
         #date = future['ds'].tail(num_days+plot_num)
@@ -98,6 +95,7 @@ def main():
         forecasted_data = viz_df.yhat_scaled
         date = future['ds']
         #date = viz_df.index[-plot_num:-1]
+        forecast_start = forecasted_data[-num_days]
 
         d = [date, close_data, forecasted_data]
         export_data = zip_longest(*d, fillvalue = '')
@@ -107,7 +105,7 @@ def main():
             wr.writerows(export_data)
         myfile.close()
 
-        return render_template("plot.html")
+        return render_template("plot.html", original = original_end, forecast = forecast_start)
 '''
 if __name__ == "__main__":
     main()
